d1b502ba08399bbd906632e5ca254bd64ab6e7d0
Red Fish <trust2001@inbox.lv>
2025-03-07 22:00:30 +0000

    Can now read files from current directory

--- Corruptor.c
+++ Corruptor.c
@@ -1,27 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
+char **files = NULL; //Array of strings
+int fileCount = 0;
 
-char *files; 
+
 
 void getDirectories()
 {
     struct dirent *de;
-
     DIR *dr = opendir(".");
+    
 
     if(dr == NULL)
     {
         printf("Couldn't open directory");
     }
 
+
     while ((de = readdir(dr)) != NULL)
     {
         char *file;
-        
 
         //printf("%s\n", de->d_name);
-        //files += (de->d_name);
+        char **temp = realloc(files, sizeof(char *) * (fileCount + 1));
+        if(temp == NULL)
+        {
+            printf("Memory reallocation failed\n");
+            closedir(dr);
+            return ;
+        }
+        files = temp;
+
+        files[fileCount] = (char *)malloc(strlen(de->d_name)+1);
+        if(files[fileCount]==NULL)
+        {
+            closedir(dr);
+            return;
+        }
+        
+        strcpy(files[fileCount], de->d_name);
+        fileCount++;
+        
+    }
+
+    for(int x = 0; x < fileCount; x++)
+    {
+        printf("%s\n", files[x]);
     }
 
     closedir(dr);
@@ -32,6 +58,11 @@ int main() {
     
     
     getDirectories();
+    for (int i = 0; i < fileCount; i++)
+    {
+        free(files[i]);
+    }
+    free(files);
     return 0;
 
 
